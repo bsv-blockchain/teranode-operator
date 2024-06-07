f2e6258301f90eaa4c6ebc45a620cfc52038032c
galt-tr <64976002+galt-tr@users.noreply.github.com>
2024-06-06 22:55:07 -0400

    Merge pull request #10 from bitcoin-sv/feature/miner
    Add miner controller for deployment

--- cmd/main.go
+++ cmd/main.go
@@ -171,6 +171,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Propagation")
 		os.Exit(1)
 	}
+	if err = (&controller.MinerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Miner")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
---
+++ internal/controller/miner_controller.go
@@ -0,0 +1,110 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"github.com/bitcoin-sv/teranode-operator/internal/utils"
+	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	apimeta "k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	teranodev1alpha1 "github.com/bitcoin-sv/teranode-operator/api/v1alpha1"
+)
+
+// MinerReconciler reconciles a Miner object
+type MinerReconciler struct {
+	client.Client
+	Scheme         *runtime.Scheme
+	Log            logr.Logger
+	NamespacedName types.NamespacedName
+	Context        context.Context
+}
+
+//+kubebuilder:rbac:groups=teranode.bsvblockchain.org,resources=miners,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=teranode.bsvblockchain.org,resources=miners/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=teranode.bsvblockchain.org,resources=miners/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Miner object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
+func (r *MinerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	result := ctrl.Result{}
+	r.Log = log.FromContext(ctx).WithValues("miner", req.NamespacedName)
+	r.Context = ctx
+	r.NamespacedName = req.NamespacedName
+	miner := teranodev1alpha1.Miner{}
+	if err := r.Get(ctx, req.NamespacedName, &miner); err != nil {
+		r.Log.Error(err, "unable to fetch miner CR")
+		return result, nil
+	}
+
+	_, err := utils.ReconcileBatch(r.Log,
+		//r.Validate,
+		// TODO: Come back and determine how to more appropriately handle overriding the configmap for each service
+		//r.ReconcileConfigMap,
+		r.ReconcileDeployment,
+	)
+
+	if err != nil {
+		apimeta.SetStatusCondition(&miner.Status.Conditions,
+			metav1.Condition{
+				Type:    teranodev1alpha1.ConditionReconciled,
+				Status:  metav1.ConditionFalse,
+				Reason:  teranodev1alpha1.ReconciledReasonError,
+				Message: err.Error(),
+			},
+		)
+	} else {
+		apimeta.SetStatusCondition(&miner.Status.Conditions,
+			metav1.Condition{
+				Type:    teranodev1alpha1.ConditionReconciled,
+				Status:  metav1.ConditionTrue,
+				Reason:  teranodev1alpha1.ReconciledReasonComplete,
+				Message: teranodev1alpha1.ReconcileCompleteMessage,
+			},
+		)
+	}
+
+	statusErr := r.Client.Status().Update(ctx, &miner)
+	if err == nil {
+		err = statusErr
+	}
+
+	return ctrl.Result{Requeue: false, RequeueAfter: 0}, err
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *MinerReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&teranodev1alpha1.Miner{}).
+		Owns(&appsv1.Deployment{}).
+		Complete(r)
+}
