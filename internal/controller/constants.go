393c4c406fbaec75ffd9851fb6ed1b015119b6cb
galt-tr <64976002+galt-tr@users.noreply.github.com>
2025-01-24 07:22:28 -0500

    Bump teranode-public version

--- internal/controller/constants.go
+++ internal/controller/constants.go
@@ -8,7 +8,7 @@ const DefaultServiceAccountName = "teranode-operator-service-runner"
 const SharedPVCName = "cluster-storage"
 
 // DefaultImage is the default teranode service image
-const DefaultImage = "434394763103.dkr.ecr.eu-north-1.amazonaws.com/teranode-public:v0.5.50"
+const DefaultImage = "434394763103.dkr.ecr.eu-north-1.amazonaws.com/teranode-public:v0.6.2"
 
 // Service Names
 
